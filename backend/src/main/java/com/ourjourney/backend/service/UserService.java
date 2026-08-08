a4a41bbb8dd8fa57e9530455f6c556d93633b688
Rosmary Garcia Pedroza <148343225+rgarciapedroza@users.noreply.github.com>
2026-08-08 11:40:16 +0200

    Revert "Feature/user registration"

--- backend/src/main/java/com/ourjourney/backend/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.ourjourney.backend.service;
-
-import com.ourjourney.backend.dto.RegisterRequest;
-import com.ourjourney.backend.dto.UserResponse;
-
-public interface UserService {
-    UserResponse register(RegisterRequest request);
-}
