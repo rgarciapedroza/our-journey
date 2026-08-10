4a81ab31315ff7d9081886d430a2ec513883de00
Rosmary <rosmarygp11@gmail.com>
2026-08-10 18:24:28 +0200

    feat: add protected current user endpoint

--- backend/src/main/java/com/ourjourney/backend/service/UserService.java
+++ backend/src/main/java/com/ourjourney/backend/service/UserService.java
@@ -8,4 +8,5 @@ import com.ourjourney.backend.dto.UserResponse;
 public interface UserService {
     UserResponse register(RegisterRequest request);
     LoginResponse login(LoginRequest request);
+    UserResponse getCurrentUser(String Email);
 }
